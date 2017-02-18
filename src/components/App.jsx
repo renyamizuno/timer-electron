36753c9cd81196643fcca361b69d8719cd2b988b
RenyaMizuno <renyamizuno@gmail.com>
2017-02-18 21:39:29 +0900

    背景色を付けた
    postcssの設定とかが正しく動くことの確認

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './app.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={style.app}>
         <div>Timer</div>
         <button>push</button>
       </div>
