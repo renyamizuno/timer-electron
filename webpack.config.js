22d887ee563fb4e4c6afa512fc045d1e767b2854
RenyaMizuno <renyamizuno@gmail.com>
2017-03-14 00:07:35 +0900

    app.jsxにリネーム
    app.js内でjsxの記法を使っていたため

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './app.js',
+  entry: './app.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
