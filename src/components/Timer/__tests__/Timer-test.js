84af2fb04f745b41699a5ab2f6ab76fb9bac8306
RenyaMizuno <renyamizuno@gmail.com>
2017-03-16 00:03:52 +0900

    TImerが時間を更新することを確認するテストを追加

--- src/components/Timer/__tests__/Timer-test.js
+++ src/components/Timer/__tests__/Timer-test.js
@@ -1,7 +1,9 @@
 import test from 'ava';
+import sinon from 'sinon';
+
 import React from 'react';
 import moment from 'moment';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Timer, { style } from '../';
 
@@ -9,4 +11,16 @@ test('Timer show current time', t => {
   const wrapper = shallow(<Timer />);
 
   t.is(wrapper.contains(<div className={style.oval}><div className={style.time}>{moment(wrapper.state().date).format('h:mm:ss')}</div></div>), true);
+});
+
+test('Timer update time', t => {
+  const clock = sinon.useFakeTimers();
+  const wrapper = mount(<Timer />);
+  const prevTime = wrapper.state('date');
+
+  clock.tick(1000);
+
+  const currentTime = wrapper.state('date');
+
+  t.not(prevTime, currentTime);
 });
